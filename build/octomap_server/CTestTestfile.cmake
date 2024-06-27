aeea080b3d64a5c2569cac085d046e4a172cf47f
sonieth <naganariselves@gmail.com>
2024-06-27 17:21:39 +0300

    feat: extra2

--- build/octomap_server/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sonieth/ros/depth_template/src/octomap_mapping/octomap_server
-# Build directory: /home/sonieth/ros/depth_template/build/octomap_server
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
